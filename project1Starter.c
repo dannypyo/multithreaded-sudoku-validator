0501aa66c0d95cdd80fd45ca812b784d997ecb76
Danny Pyo <jdannypyo@gmail.com>
2026-02-23 08:32:53 -0700

    sudokupuzzle reader

--- project1Starter.c
+++ project1Starter.c
@@ -6,8 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <stdbool.h>
 
-typedef int bool;
 #define TRUE 1
 #define FALSE 0
 
@@ -16,6 +16,7 @@ int sudokuPuzzle[9][9];
 int colArray[9];
 int rowArray[9];
 int subgridArray[9];
+char buffer[81];
 
 void *colChecker(void *param);
 void *rowChecker(void *param);
@@ -30,7 +31,28 @@ typedef struct{
 } parameters;
 
 int main(){
-	
+	FILE *fptr;
+	int row = 0;
+	fptr = fopen("C:\\Users\\Daniel Pyo\\CS3\\project-01-team-flames\\SudokuPuzzle.txt", "r");
+	while (fgets(buffer, sizeof(buffer), fptr) != NULL){
+		sscanf(buffer, "%d %d %d %d %d %d %d %d %d", 
+			&sudokuPuzzle[row][0], 
+			&sudokuPuzzle[row][1], 
+			&sudokuPuzzle[row][2], 
+			&sudokuPuzzle[row][3], 
+			&sudokuPuzzle[row][4], 
+			&sudokuPuzzle[row][5], 
+			&sudokuPuzzle[row][6], 
+			&sudokuPuzzle[row][7],
+			&sudokuPuzzle[row][8]);
+		row++;
+	}
+	for (int i = 0; i < 9; i++) {
+    	for (int j = 0; j < 9; j++) {
+        printf("%d ", sudokuPuzzle[i][j]); // print each number
+    	}
+    printf("\n"); // new line after each row
+	}
 	
 }
 
