77b46fd7f8da82b3d67ee95c31533ebb342a5594
JaidenSearle <jsearle6@msudenver.edu>
2026-03-04 08:24:25 -0700

    Initialized boolean arrays and added subgrid data

--- project1Starter.c
+++ project1Starter.c
@@ -1,12 +1,11 @@
 /**
-*<Program header>
-*/
+ *<Program header>
+ */
 
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define TRUE 1
 #define FALSE 0
@@ -18,56 +17,64 @@ int rowArray[9];
 int subgridArray[9];
 char buffer[81];
 
-void *colChecker(void *param);
-void *rowChecker(void *param);
-void *subgridChecker(void *param);
+void* colChecker(void* param);
+void* rowChecker(void* param);
+void* subgridChecker(void* param);
 
 //
-typedef struct{
-	int topRow;
-	int bottomRow;
-	int leftColumn;
-	int rightColumn;
+typedef struct {
+  int topRow;
+  int bottomRow;
+  int leftColumn;
+  int rightColumn;
 } parameters;
 
-int main(){
-	FILE *fptr;
-	int row = 0;
-	fptr = fopen("SudokuPuzzle.txt", "r");
-	while (fgets(buffer, sizeof(buffer), fptr) != NULL){
-		sscanf(buffer, "%d %d %d %d %d %d %d %d %d", 
-			&sudokuPuzzle[row][0], 
-			&sudokuPuzzle[row][1], 
-			&sudokuPuzzle[row][2], 
-			&sudokuPuzzle[row][3], 
-			&sudokuPuzzle[row][4], 
-			&sudokuPuzzle[row][5], 
-			&sudokuPuzzle[row][6], 
-			&sudokuPuzzle[row][7],
-			&sudokuPuzzle[row][8]);
-		row++;
-	}
-	for (int i = 0; i < 9; i++) {
-    	for (int j = 0; j < 9; j++) {
-        printf("%d ", sudokuPuzzle[i][j]); // print each number
-    	}
-    printf("\n"); // new line after each row
-	}
-	
+int main() {
+  FILE* fptr;
+  int row = 0;
+
+  fptr = fopen("SudokuPuzzle.txt", "r");
+
+  while (fgets(buffer, sizeof(buffer), fptr) != NULL) {
+    sscanf(buffer, "%d %d %d %d %d %d %d %d %d", &sudokuPuzzle[row][0],
+           &sudokuPuzzle[row][1], &sudokuPuzzle[row][2], &sudokuPuzzle[row][3],
+           &sudokuPuzzle[row][4], &sudokuPuzzle[row][5], &sudokuPuzzle[row][6],
+           &sudokuPuzzle[row][7], &sudokuPuzzle[row][8]);
+    row++;
+  }
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      printf("%d ", sudokuPuzzle[i][j]);  // print each number
+    }
+    printf("\n");  // new line after each row
+  }
+  // initialize all entries in Boolean arrays to false
+  for (int i = 0; i < 9; i++) {
+    colArray[i] = FALSE;
+  }
+  for (int i = 0; i < 9; i++) {
+    rowArray[i] = FALSE;
+  }
+  for (int i = 0; i < 9; i++) {
+    subgridArray[i] = FALSE;
+  }
+  // subgrid data
+  parameters subgridData[9];
+  int index = 0;
+
+  for (int row = 0; row < 9; row += 3) {
+    for (int col = 0; col < 9; col += 3) {
+      subgridData[index].topRow = row;
+      subgridData[index].bottomRow = row + 2;
+      subgridData[index].leftColumn = col;
+      subgridData[index].rightColumn = col + 2;
+      index++;
+    }
+  }
 }
 
-void *colChecker(void *param){
-	
+void* colChecker(void* param) {}
 
+void* rowChecker(void* param) {}
 
-}	
-
-void *rowChecker(void *param){
-
-
-}
-
-void *subgridChecker(void *param){
-
-
-}
+void* subgridChecker(void* param) {}
